d6fbe40a8a739799b38f262e9d52ca4830653844
Vinay Bhat <vibha@microsoft.com>
2019-02-21 14:31:10 +0530

    Encryption using Certs

--- PolicyStorageService/EncryptionUtil.cs
+++ PolicyStorageService/EncryptionUtil.cs
@@ -7,6 +7,8 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Runtime.Remoting.Contexts;
 using System.Fabric;
+using System.Runtime.InteropServices;
+using System.Security;
 
 namespace PolicyStorageService
 {
@@ -14,56 +16,38 @@ namespace PolicyStorageService
     {
         public static string Encrypt(string clearText)
         {
-            string EncryptionKey = GetEncryptionKey();
-            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
-            using (Aes encryptor = Aes.Create())
-            {
-                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
-                encryptor.Key = pdb.GetBytes(32);
-                encryptor.IV = pdb.GetBytes(16);
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
-                    {
-                        cs.Write(clearBytes, 0, clearBytes.Length);
-                        cs.Close();
-                    }
-                    clearText = Convert.ToBase64String(ms.ToArray());
-                }
-            }
-            return clearText;
-        }
-        public static string Decrypt(string cipherText)
-        {
-            string EncryptionKey = GetEncryptionKey();
-            cipherText = cipherText.Replace(" ", "+");
-            byte[] cipherBytes = Convert.FromBase64String(cipherText);
-            using (Aes encryptor = Aes.Create())
-            {
-                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
-                encryptor.Key = pdb.GetBytes(32);
-                encryptor.IV = pdb.GetBytes(16);
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
-                    {
-                        cs.Write(cipherBytes, 0, cipherBytes.Length);
-                        cs.Close();
-                    }
-                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
-                }
-            }
-            return cipherText;
+            var thumbprint = GetCertThumbprint();
+            return System.Fabric.Security.EncryptionUtility.EncryptText(clearText, thumbprint, "My");
         }
 
-        public static string GetEncryptionKey()
+        public static string Decrypt(string cipherText)
+        {
+            // TODO: Make users of Decrypt use SecureString instead
+            return SecureStringToString(System.Fabric.Security.EncryptionUtility.DecryptText(cipherText));
+        }
+
+        public static string SecureStringToString(SecureString value)
+        {
+            IntPtr valuePtr = IntPtr.Zero;
+            try
+            {
+                valuePtr = Marshal.SecureStringToGlobalAllocUnicode(value);
+                return Marshal.PtrToStringUni(valuePtr);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(valuePtr);
+            }
+        }
+
+        public static string GetCertThumbprint()
         {
             CodePackageActivationContext activationContext = FabricRuntime.GetActivationContext();
             var configurationPackage = activationContext.GetConfigurationPackageObject("Config");
 
-            string policyStorageEncryptionKey = configurationPackage.Settings.Sections["PolicyStorageSecurityConfig"].Parameters["PolicyStorageEncryptionKey"].Value;
+            string policyStorageCertThumbprint = configurationPackage.Settings.Sections["PolicyStorageSecurityConfig"].Parameters["PolicyStorageCertThumbprint"].Value;
 
-            return policyStorageEncryptionKey;
+            return policyStorageCertThumbprint;
         }
     }
 }
